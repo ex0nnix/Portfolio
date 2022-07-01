901c2fb1f5f54c4b552e3c0d10be70c3ad7ad5e9
Exonnix Kai <77865925+ex0nnix@users.noreply.github.com>
2022-07-01 03:02:41 -0700

    file upload

---
+++ src/components/sections/about.js
@@ -0,0 +1,194 @@
+import React, { useEffect, useRef } from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
+import Img from 'gatsby-image';
+import styled from 'styled-components';
+import { srConfig } from '@config';
+import sr from '@utils/sr';
+
+const StyledAboutSection = styled.section`
+  max-width: 900px;
+
+  .inner {
+    display: grid;
+    grid-template-columns: 3fr 2fr;
+    grid-gap: 50px;
+
+    @media (max-width: 768px) {
+      display: block;
+    }
+  }
+`;
+const StyledText = styled.div`
+  ul.skills-list {
+    display: grid;
+    grid-template-columns: repeat(2, minmax(140px, 200px));
+    padding: 0;
+    margin: 20px 0 0 0;
+    overflow: hidden;
+    list-style: none;
+
+    li {
+      position: relative;
+      margin-bottom: 10px;
+      padding-left: 20px;
+      font-family: var(--font-mono);
+      font-size: var(--fz-xs);
+
+      &:before {
+        content: '▹';
+        position: absolute;
+        left: 0;
+        color: var(--green);
+        font-size: var(--fz-sm);
+        line-height: 12px;
+      }
+    }
+  }
+`;
+const StyledPic = styled.div`
+  position: relative;
+  max-width: 300px;
+
+  @media (max-width: 768px) {
+    margin: 50px auto 0;
+    width: 70%;
+  }
+
+  .wrapper {
+    ${({ theme }) => theme.mixins.boxShadow};
+    display: block;
+    position: relative;
+    width: 100%;
+    border-radius: var(--border-radius);
+    background-color: var(--green);
+
+    &:hover,
+    &:focus {
+      background: transparent;
+      outline: 0;
+
+      &:after {
+        top: 15px;
+        left: 15px;
+      }
+
+      .img {
+        filter: none;
+        mix-blend-mode: normal;
+      }
+    }
+
+    .img {
+      position: relative;
+      border-radius: var(--border-radius);
+      mix-blend-mode: multiply;
+      filter: grayscale(100%) contrast(1);
+      transition: var(--transition);
+    }
+
+    &:before,
+    &:after {
+      content: '';
+      display: block;
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      border-radius: var(--border-radius);
+      transition: var(--transition);
+    }
+
+    &:before {
+      top: 0;
+      left: 0;
+      background-color: var(--navy);
+      mix-blend-mode: screen;
+    }
+
+    &:after {
+      border: 2px solid var(--green);
+      top: 20px;
+      left: 20px;
+      z-index: -1;
+    }
+  }
+`;
+
+const About = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      avatar: file(sourceInstanceName: { eq: "images" }, relativePath: { eq: "ex0nnix.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 500, traceSVG: { color: "#64ffda" }) {
+            ...GatsbyImageSharpFluid_withWebp_tracedSVG
+          }
+        }
+      }
+    }
+  `);
+
+  const revealContainer = useRef(null);
+
+  useEffect(() => {
+    sr.reveal(revealContainer.current, srConfig());
+  }, []);
+
+  const skills = [
+    'Solidity',
+    'JS/TS',
+    'NFT',
+    'React',
+    'Vue',
+    'Svelte',
+    'NodeJS',
+    'GraphQL',
+    'Hardhat',
+    'Truffle',
+    'OpenZeppelin',
+    'AWS',
+    'GoLang',
+    'Python',
+  ];
+
+  return (
+    <StyledAboutSection id="about" ref={revealContainer}>
+      <h2 className="numbered-heading">About Me</h2>
+
+      <div className="inner" style={{fontFamily: 'Didact Gothic'}}>
+        <StyledText>
+          <div style={{fontFamily: 'Didact Gothic'}}>
+            <p>
+              Hello! My name is Lao and I enjoy creating things that live on the internet. My
+              interest in web development started back in 2009 when I was at college.
+            </p>
+
+            <p>
+            I am a senior software engineer with more than 10 years of experience with Software Engineering, specialized in Blockchain and Fullstack engineering.
+So far, I have been committed to many enterprise-level projects and also worked with various startup companies. Since I am a big fan of Blockchain(especially super passionate about NFT), I have rich experience with several blockchain platforms like Ethereum and Solana. DApp development is one of my best move with web3 integration. Also, I have always dedicated to meet the customers' satisfaction and their requirements.
+            </p>
+
+            <p>
+            I always love what I do and I do what I love. No matter what I am working on I am very invested. Because of this, in my previous teams, the teammates and the manager described me as a pro-active team player, fast learner, and a problem solver. I know that communication and collaboration are the keys to the success of the project. So I always emphasize the importance of listening carefully to the requirements and details.
+            </p>
+            <p>
+              Outside of work, I enjoy fitness training and listening to music since I found that they are wonderful stress-reliever.
+            </p>
+
+            <p>Here are a few technologies I've been working with recently:</p>
+          </div>
+
+          <ul className="skills-list">
+            {skills && skills.map((skill, i) => <li key={i}>{skill}</li>)}
+          </ul>
+        </StyledText>
+
+        <StyledPic>
+          <div className="wrapper">
+            <Img fluid={data.avatar.childImageSharp.fluid} alt="Avatar" className="img" />
+          </div>
+        </StyledPic>
+      </div>
+    </StyledAboutSection>
+  );
+};
+
+export default About;
